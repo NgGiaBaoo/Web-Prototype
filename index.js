d1347b3b8c8400f0fd6ba9b68e26915ade5cb52b
Nguyen Gia Bao <24100074@st.phenikaa-uni.edu.vn>
2026-05-26 07:52:42 +0000

    hh

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello Express Server!");
+app.get('/api/get', (req, res) => {
+
+    res.json({
+        message: 'This is a GET request!'
+    });
+
 });
 
+app.post('/api/post', (req, res) => {
+
+    console.log(req.body);
+
+    res.json({
+        message: 'This is a POST request!',
+        data: req.body
+    });
+
+});
+
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
